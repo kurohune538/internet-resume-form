cbac26dffd78837421eab13fb9b0b565f406e79c
Shinnosuke Komiya <kurohune538@gmail.com>
2020-10-13 23:44:57 -0400

    add windows

--- internet-resume/src/components/window/index.js
+++ internet-resume/src/components/window/index.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import Draggable, { DraggableCore } from "react-draggable"
 
-const Window = ({ position }) => {
+const windowGap = 5
+const Window = ({ position, width, height }) => {
+  const [dragged, changeDragState] = useState(false)
   const handleStart = () => {
     console.log("handleStart")
+    changeDragState(true)
   }
   const handleDrag = () => {
     console.log("handleDrag")
@@ -19,25 +22,77 @@ const Window = ({ position }) => {
       onStop={() => handleStop()}
       handle="strong"
     >
-      <StyledWindow position={position}>
-        <strong>
-          <StatusBar>
-            <Icons>
-              <Icon bg=""></Icon>
-              <Icon bg=""></Icon>
-              <Icon bg=""></Icon>
-            </Icons>
-          </StatusBar>
-        </strong>
-        <InsideWindow>hoge</InsideWindow>
-      </StyledWindow>
+      <div>
+        <StyledWindow position={position} width={width} height={height}>
+          <strong>
+            <StatusBar>
+              <Icons>
+                <Icon bg=""></Icon>
+                <Icon bg=""></Icon>
+                <Icon bg=""></Icon>
+              </Icons>
+            </StatusBar>
+          </strong>
+          <InsideWindow>hoge</InsideWindow>
+        </StyledWindow>
+        {dragged && (
+          <>
+            <BugWindow
+              width={width}
+              height={height}
+              zIndex={-1}
+              position={{
+                x: position.x + windowGap,
+                y: position.y + windowGap,
+              }}
+            />
+            <BugWindow
+              width={width}
+              height={height}
+              zIndex={-2}
+              position={{
+                x: position.x + windowGap * 2,
+                y: position.y + windowGap * 2,
+              }}
+            />
+            <BugWindow
+              width={width}
+              height={height}
+              zIndex={-3}
+              position={{
+                x: position.x + windowGap * 3,
+                y: position.y + windowGap * 3,
+              }}
+            />
+
+            <BugWindow
+              width={width}
+              height={height}
+              zIndex={-4}
+              position={{
+                x: position.x + windowGap * 4,
+                y: position.y + windowGap * 4,
+              }}
+            />
+            <BugWindow
+              width={width}
+              height={height}
+              zIndex={-5}
+              position={{
+                x: position.x + windowGap * 5,
+                y: position.y + windowGap * 5,
+              }}
+            />
+          </>
+        )}
+      </div>
     </Draggable>
   )
 }
 
 const StyledWindow = styled.div`
-  width: 300px;
-  height: 250px;
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
   position: absolute;
   left: ${props => props.position.x}px;
   top: ${props => props.position.y}px;
@@ -78,6 +133,18 @@ const Icon = styled.div`
   width: 15px;
   height: 15px;
   margin: 0 2px;
+`
+
+const BugWindow = styled.div`
+  position: absolute;
+  left: ${props => props.position.x}px;
+  top: ${props => props.position.y}px;
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+  border: solid 1px #666;
+  box-sizing: border-box;
+  z-index: ${props => props.zIndex};
+  background: #fff;
 `
 
 export default Window
--- internet-resume/src/pages/index.js
+++ internet-resume/src/pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   const { width, height } = useWindowSize()
   return (
     <>
-      <Window position={{ x: 100, y: 100 }} />
-      <Window position={{ x: 200, y: 200 }} />
+      <Window position={{ x: 100, y: 100 }} width={300} height={250} />
+      <Window position={{ x: 200, y: 200 }} width={300} height={250} />
     </>
   )
 }
